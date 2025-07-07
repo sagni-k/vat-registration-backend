7084bc2c0e62428989e6ee4bff9ec9b3809fe8d7
sagni-k <sagnikkarmakar10@gmail.com>
2025-07-08 01:48:51 +0530

    circular dependency

--- src/main/java/com/nic/vat/registration/config/security/SecurityConfig.java
+++ src/main/java/com/nic/vat/registration/config/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.nic.vat.registration.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,6 +26,7 @@ public class SecurityConfig {
     private JwtAuthEntryPoint jwtAuthEntryPoint;
 
     @Autowired
+    @Lazy
     private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     /**
