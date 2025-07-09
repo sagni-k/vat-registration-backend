62cf0f6a0ded383d0ad4450648e598f48f23ec22
sagni-k <sagnikkarmakar10@gmail.com>
2025-07-09 12:39:11 +0530

    added /registration/documents: and /registration/submit:

--- src/main/java/com/nic/vat/registration/repository/DealerDocumentRepository.java
+++ src/main/java/com/nic/vat/registration/repository/DealerDocumentRepository.java
@@ -4,5 +4,9 @@ import com.nic.vat.registration.model.DealerDocument;
 import com.nic.vat.registration.model.DealerDocumentId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface DealerDocumentRepository extends JpaRepository<DealerDocument, DealerDocumentId> {
+    List<DealerDocument> findByAckNo(BigDecimal ackNo);
 }
