63e99c2f25cb8de550bb037590672fcfb65bbf63
sagni-k <sagnikkarmakar10@gmail.com>
2025-07-10 23:22:08 +0530

    options

--- src/main/java/com/nic/vat/registration/config/WebConfig.java
+++ src/main/java/com/nic/vat/registration/config/WebConfig.java
@@ -14,10 +14,13 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://vat-registration-frontend.vercel.app")
+                        .allowedOriginPatterns(
+                                "http://localhost:3000",
+                                "https://vat-registration-frontend.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true); // If frontend uses cookies or auth headers
+                        .allowCredentials(true);
             }
         };
     }
