5ef9cccd38058b93b1572311868ac47e50b5c1a6
sagni-k <sagnikkarmakar10@gmail.com>
2025-07-17 01:45:02 +0530

    partner get format correct2

--- src/main/java/com/nic/vat/registration/controller/PartnerController.java
+++ src/main/java/com/nic/vat/registration/controller/PartnerController.java
@@ -1,6 +1,7 @@
 package com.nic.vat.registration.controller;
 
 import com.nic.vat.registration.model.dto.PartnerRequest;
+import com.nic.vat.registration.model.dto.PartnerResponse;
 import com.nic.vat.registration.model.DealerPartner;
 import com.nic.vat.registration.service.PartnerService;
 import org.springframework.beans.factory.annotation.Autowired;
